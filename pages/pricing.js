5e908f665d0fd3658450d02b7aff61c9c0a77d1e
Dapsz-ID <dapszzaja@gmail.com>
2025-08-27 16:58:08 +0700

    Create pricing.js

---
+++ pages/pricing.js
@@ -0,0 +1,2 @@
+import Layout from '@/components/Layout'
+export default function Pricing(){ return (<Layout><div className='container py-16'><h1 className='text-4xl font-bold text-center'>Paket Hosting</h1></div></Layout>) }
