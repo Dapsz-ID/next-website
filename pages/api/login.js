856849083a09bc7d6e89ca32c8e24f637c1ffb33
Dapsz-ID <dapszzaja@gmail.com>
2025-08-27 17:02:41 +0700

    Create login.js

---
+++ pages/api/login.js
@@ -0,0 +1,2 @@
+import users from '@/data/users.json'
+export default function handler(req,res){ if(req.method!=='POST') return res.status(405).json({message:'Method not allowed'}); const {username,password} = req.body || {}; const user = users.find(u=>u.username===username && u.password===password); if(user) return res.status(200).json({success:true,message:'Login berhasil', user:{username:user.username,role:user.role,verified:user.verified,avatar:user.avatar,banner:user.banner}}); return res.status(401).json({success:false,message:'Username atau password salah'}) }
